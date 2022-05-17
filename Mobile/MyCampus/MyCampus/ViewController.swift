7a5bdf9fcddd7082c663d53ad354a321eb125234
demonsheart <a2509875617@gmail.com>
2022-05-17 16:27:31 +0800

    init MyCampus

---
+++ Mobile/MyCampus/MyCampus/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  MyCampus
+//
+//  Created by aicoin on 2022/5/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .cyan
+    }
+}
+
